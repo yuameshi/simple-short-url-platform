ac768d67955cd416119b9f07f3de1446abb477b7
糸 <64469437+Yuameshi@users.noreply.github.com>
2022-12-13 17:54:18 -0800

    TS!!!TS!!!!!!

--- index.ts
+++ index.ts
@@ -1,31 +1,30 @@
 // 使用DOM API获取页面中的元素
 const form = document.getElementById('form-generate');
 const inputLink = document.getElementById('input-link');
-const buttonGenerate = document.getElementById('button-generate');
 const linkResult = document.getElementById('link-result');
 const shortLink = document.getElementById('short-link');
 
 // 为表单提交事件添加处理函数
-form.addEventListener('submit', async (event) => {
+form?.addEventListener('submit', async (e) => {
 	// 阻止表单默认的提交事件
-	event.preventDefault();
+	e.preventDefault();
 
 	// 获取用户输入的原始链接
-	const originalLink = inputLink.value;
+	const originalLink = inputLink?.value;
 
 	// 在这里处理原始链接，生成短链接
 	// 这里假设已经生成了一个短链接，结果保存在变量shortLinkValue中
 	const XHR = new XMLHttpRequest();
 	XHR.open('POST', 'https://糸.cf/');
-	XHR.addEventListener('load', (event) => {
-		const shortLinkValue = event.target.responseText;
+	XHR.addEventListener('load', (e) => {
+		const shortLinkValue = e.target.responseText;
 		// 更新页面中的短链接显示区域
-		shortLink.href = shortLinkValue;
-		shortLink.textContent = shortLinkValue;
-		linkResult.classList.remove('mdui-hidden');
+		shortLink?.href = shortLinkValue;
+		shortLink?.innerText = shortLinkValue;
+		linkResult?.classList.remove('mdui-hidden');
 
 		// 清空表单中的输入
-		inputLink.value = '';
+		inputLink?.value = '';
 		mdui.refreshTextFields();
 	});
 	XHR.send(originalLink);
